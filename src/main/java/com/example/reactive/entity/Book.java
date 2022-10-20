feca3cfb00627ddf31d602b852813579c2203c67
khodyko <I.Khodyko@isida.by>
2022-10-20 11:50:44 +0300

    fix

--- src/main/java/com/example/reactive/entity/Book.java
+++ src/main/java/com/example/reactive/entity/Book.java
@@ -6,10 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+import org.springframework.data.relational.core.mapping.Table;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table
 public class Book {
     @Id
     private Long id;
